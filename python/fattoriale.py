4ec2100d6c0b2b09a1e4b65dcb4e8fbc662752b0
nicolas1617 <nicolasminervini@gmail.com>
2018-05-19 18:08:31 +0200

    modificato fattoriale.py

--- python/fattoriale.py
+++ python/fattoriale.py
@@ -1,16 +1,15 @@
 while True:
-        print "Questo programma calcola il fattoriale di un numero"
-        print ""
-        a = input("Inserisci il numero: ")
-        b = a
-        while b > 1:
-                b = b-1
-                a = a*b
-        print "il fattoriale del numero e' ",a
-        print ""
-        c = raw_input ("premi un qualsiasi tasto per ricominciare o scrivi no per uscire ")
-        if c == "no":
-                exit()
-        else:
-                print ""
-        
+	print "Questo programma calcola il fattoriale di un numero"
+	print ""
+	a = input("Inserisci il numero: ")
+	b = a
+	while b > 1:
+		b = b-1
+		a = a*b
+	print "il fattoriale del numero e' ",a
+	print ""
+	c = raw_input ("premi un qualsiasi tasto per ricominciare o scrivi no per uscire ")
+	if c == "no":
+		exit()
+	else:
+		print ""
